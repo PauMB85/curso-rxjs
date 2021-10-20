b6c3861bd08da1d245b3c1e0795590072bcdcacc
PauMB <pau.maravi@devo.com>
2021-10-20 09:52:16 +0200

    operadores

--- code-examples/src/index.ts
+++ code-examples/src/index.ts
@@ -1,43 +1,17 @@
-import { of, from } from 'rxjs';
+import { fromEvent, range } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//Ejemplo 1
+/*range(1,5).pipe(
+  map<number, number>(val => val * 10)
+)
+.subscribe(console.log);*/
 
 
-/**
- * of = toma argumentos y genera una secuencia de valores
- * from = crear un observable desde un array, promesa, otro observable
- */
+//Ejemplo 2
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+const keyupCode$ = keyup$.pipe(
+  map(event => event.code)
+)
 
-const observer = {
-  next: val => console.log('next:', val),
-  complete: () => console.log('complete')
-};
-
-//Ej-1
-//const source$ = from([1,2,3,4,5]);
-
-//Ej-2
-//const source$ = of([1,2,3,4,5]);
-//const source$ = of(...[1,2,3,4,5]);
-
-//Ej-3
-//const source$ = from('Pau');
-
-//Ej-4
-/*const source$ = from(fetch('https://api.github.com/users/paumb85'));
-source$.subscribe(async(resp) => {
-  console.log(resp);
-  const data = await resp.json();
-  console.log(data);
-});
-*/
-//Ej-5
-//utilizando iteradores, nos permite obtener los valores de forma secuencial
-const miGenerador =  function*() {
-  yield 1;
-  yield 2;
-  yield 3;
-  yield 4;
-  yield 5;
-};
-
-const miIterable = miGenerador();
-from(miIterable).subscribe(observer);
+keyupCode$.subscribe( code => console.log('map', code));
