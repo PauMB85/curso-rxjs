dafcbfb0c19753c5153a38865fb127f2621a6ef0
PauMB <pau.maravi@devo.com>
2021-11-05 17:56:42 +0100

    operadores combinacion

---
+++ code-examples/src/07_combinacion_operadores/01_startWith.ts
@@ -0,0 +1,6 @@
+import { of } from "rxjs";
+import {startWith} from 'rxjs/operators';
+
+of(1,2,3).pipe(
+  startWith(0)
+).subscribe(console.log)
--- code-examples/src/07_combinacion_operadores/02_endWith.ts
+++ code-examples/src/07_combinacion_operadores/02_endWith.ts
@@ -1,6 +1,6 @@
 import { of } from "rxjs";
 import {endWith} from 'rxjs/operators';
 
-const numeros$ = of(1,2,3).pipe(
+of(1,2,3).pipe(
   endWith(4)
 ).subscribe(console.log)
